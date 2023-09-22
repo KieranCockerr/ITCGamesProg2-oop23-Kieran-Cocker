3e7f4c9f84c988bcc2708e989b56ac961c9277ac
Kieran_Cocker <C00259317@setu.ie>
2023-09-22 09:30:36 +0100

    Added a range loop

--- Lab Exercise 1/Lab Exercise 1/Lab1q1.cpp
+++ Lab Exercise 1/Lab Exercise 1/Lab1q1.cpp
@@ -6,5 +6,13 @@ int main()
 	int arrayA[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Creating a new array with the specified elements
 	int arrayAptr = *arrayA; // Creating a variable that is the pointer of the array
 
-	std::cout << "The memory location of the array is " << arrayA << "\n";
+	std::cout << "The memory location of the array is " << arrayA << "\n"; // Print the location of arrayA in memory
+	std::cout << "The size of the array is " << sizeof(arrayA) << "\n"; // Print the size of the array in memory
+
+	// range loop
+	for (int value : arrayA)
+	{
+		value++; // Add +1 to each element of the array
+		std::cout << value << "\n"; // Display all the elements of the array
+	}
 }
