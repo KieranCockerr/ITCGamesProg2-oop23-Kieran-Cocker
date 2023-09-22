f8a81af1f21efc67a39531db22ce2a190b2446db
Kieran_Cocker <C00259317@setu.ie>
2023-09-22 10:54:43 +0100

    Started Q5

---
+++ Lab Exercise 1/Lab Exercise 1/Lab1q4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	// Creating a new enum class for inventory items
+	enum class InventoryItems {
+		NONE,
+		HEALTH_PACK,
+		POWER_UP,
+		SILVER_PACK,
+		GOLD_COINS
+	};
+
+	InventoryItems invArray[5]; // An array containing the inventory items
+
+	for (InventoryItems value : invArray)
+	{
+		// Unfinished
+	}
+}
