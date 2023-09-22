00f5eae628878f77db094a0c819c086fc62571b1
Kieran_Cocker <C00259317@setu.ie>
2023-09-22 09:25:05 +0100

    Added array and pointer

--- Lab Exercise 1/Lab Exercise 1/Lab1q1.cpp
+++ Lab Exercise 1/Lab Exercise 1/Lab1q1.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
+	
+	int arrayA[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Creating a new array with the specified elements
+	int arrayAptr = *arrayA; // Creating a variable that is the pointer of the array
 
+	std::cout << "The memory location of the array is " << arrayA << "\n";
 }
